9d85648ab092b05b54443518bb8517a60ecff5fe
Pola <your-email@example.com>
2026-02-01 21:57:24 +0100

    first commit

--- next.config.js
+++ next.config.js
@@ -6,14 +6,6 @@ const nextConfig = {
     domains: ['xapzapolami.b-cdn.net', 'storage.bunnycdn.com'],
     unoptimized: true
   },
-  experimental: {
-    appDir: true,
-  },
-  env: {
-    NEXT_PUBLIC_APPWRITE_ENDPOINT: process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT,
-    NEXT_PUBLIC_APPWRITE_PROJECT_ID: process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID,
-    NEXT_PUBLIC_BUNNY_CDN_BASE_URL: process.env.NEXT_PUBLIC_BUNNY_CDN_BASE_URL,
-  }
 }
 
 module.exports = nextConfig
