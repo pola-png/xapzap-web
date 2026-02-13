4ef1c4b325aad75fc0b26871ffec0c640f8f40b3
Pola <your-email@example.com>
2026-02-14 00:23:32 +0100

    descri[tion and views more button change

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,19 @@ const nextConfig = {
   images: {
     unoptimized: true
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=0, must-revalidate',
+          },
+        ],
+      },
+    ]
+  },
   async rewrites() {
     return [
       {
