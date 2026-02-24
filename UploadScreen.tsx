44ba5723aa9eff6891750eaf53fec90b04ddd429
Pola <your-email@example.com>
2026-02-24 17:53:01 +0100

    descri[tion and views more button change

--- UploadScreen.tsx
+++ UploadScreen.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useRef, useEffect } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import { useRouter } from 'next/navigation'
 import { Upload, X, Video, Image, Newspaper, Film, Plus, Loader2, Sparkles } from 'lucide-react'
 import appwriteService from './appwriteService'
